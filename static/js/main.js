d3ab2c97e003f990a2bbad6d2eb55ae1830312ce
Souken521 <west.haru6555@gmail.com>
2026-06-22 13:47:11 +0900

    ヘッダーを仮実装(テンプレ利用)
    https://tips-log.com/header-responsive_template/#1

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburger = document.querySelector('.c-hamburger');
+  const headNav = document.querySelector('.p-header__nav');
+
+
+  if (!hamburger) {
+    return false;
+  }
+
+  hamburger.addEventListener('click', () => {
+    if (hamburger.classList.contains("is-active")) {
+      hamburger.classList.remove('is-active');
+      headNav.classList.remove('is-active')
+    } else {
+      hamburger.classList.add('is-active');
+      headNav.classList.add('is-active')
+    }
+
+  });
+
+  headNav.addEventListener('click', () => {
+    hamburger.classList.remove('is-active');
+    headNav.classList.remove('is-active')
+  });
+});
