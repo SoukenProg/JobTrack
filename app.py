e44b6857d718cd3ee3976b87d1f3cc416fdbd1c0
Souken521 <west.haru6555@gmail.com>
2026-06-24 14:57:01 +0900

    コマンドを実行できるようにする

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import os
 from pathlib import Path
 from flask import Flask, render_template
 from models import db
+from commands import register_commands
 
 from routes.dashboard import bp as dashboard_bp
 from routes.companies import bp as company_bp
 
 def create_app():
     app = Flask(__name__)
+    # DBを登録
     database_path = Path(app.instance_path) / "JobTracker.db"
     app.config.from_mapping(
         SECRET_KEY=os.environ.get("SECRET_KEY", "dev-change-me"),
@@ -19,6 +21,10 @@ def create_app():
 
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(company_bp)
+
+    # コマンドを登録
+    register_commands(app)
+
     with app.app_context():
         db.create_all()
     return app
