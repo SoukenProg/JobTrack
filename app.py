12e130682b705e3d02a322f18aa2997b4e746bb8
Souken521 <west.haru6555@gmail.com>
2026-06-24 11:40:40 +0900

    ダッシュボードの仮画面実装

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+# モックデータ
+stats ={
+    "companies": 0,
+    "active": 0,
+    "interview":0,
+    "success":0,
+}
 @app.route('/')
 def top():
-    return render_template('index.html')
+    return render_template('index.html',stats=stats)
 
 
 if __name__ == '__main__':
