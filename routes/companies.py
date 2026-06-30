718460982ab3d96c197c74a0192bc00c1ced46f5
Souken521 <west.haru6555@gmail.com>
2026-06-30 12:23:44 +0900

    企業登録機能を仮作成

--- routes/companies.py
+++ routes/companies.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import select
 
-from models import Companies, db
+from models import Applications, Companies, db
 
 
 bp = Blueprint("companies", __name__, url_prefix="/companies")
@@ -33,3 +33,31 @@ def new():
         return redirect(url_for("companies.index"))
 
     return render_template("companies/new.html")
+
+@bp.route("/<int:company_id>")
+def detail(company_id):
+    company = db.get_or_404(Companies, company_id)
+    return render_template("companies/detail.html")
+
+@bp.route("/<int:company_id>/edit", methods=["GET", "POST"])
+def edit(company_id):
+    company = db.get_or_404(Companies, company_id)
+
+    if request.method == "POST":
+        company.company_name = request.form["company_name"].strip()
+        company.industry = request.form["industry"].strip()
+        company.website = request.form["website"].strip()
+        company.location = request.form["location"].strip()
+        company.memo = request.form["memo"].strip()
+
+        db.session.add(company)
+        db.session.commit()
+        return redirect(url_for("companies.detail", company_id=company_id))
+    return render_template("companies/edit.html", company=company)
+
+@bp.route("/<int:company_id>/delete", methods=["POST"])
+def delete(company_id):
+    company = db.get_or_404(Companies, company_id)
+    db.session.delete(company)
+    db.session.commit()
+    return redirect(url_for("companies.index"))
