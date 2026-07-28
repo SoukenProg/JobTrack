72a8f1f63ceb936a91f58de2ae4268d47b0685a4
Souken521 <west.haru6555@gmail.com>
2026-07-28 13:51:29 +0900

    ダッシュボードにリンクを追加

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -11,27 +11,51 @@ bp = Blueprint("dashboard", __name__)
 @bp.route("/")
 def index():
     # 渡すデータ
-    applications = db.session.execute(
-        select(Applications)
-        .join(Companies)
-        .order_by(Applications.created_at.desc())
-        .limit(10)
-    ).scalars().all()
-    interviews = db.session.execute(
-        select(Interviews)
-        .where(Interviews.interview_date >= datetime.now())
-        .order_by(Interviews.interview_date.asc())
-    ).scalars().all()
+    applications = (
+        db.session.execute(
+            select(Applications)
+            .join(Companies)
+            .order_by(Applications.created_at.desc())
+            .limit(10)
+        )
+        .scalars()
+        .all()
+    )
+    interviews = (
+        db.session.execute(
+            select(Interviews)
+            .where(Interviews.interview_date >= datetime.now())
+            .order_by(Interviews.interview_date.asc())
+            .limit(5)
+        )
+        .scalars()
+        .all()
+    )
     active_status = APPLICATION_STATUSES[1:6]
     # 各段階の数
     stats = {
         "companies": db.session.scalar(select(func.count()).select_from(Companies)),
         "active": db.session.scalar(
-            select(func.count()).select_from(Applications).where(Applications.status.in_(active_status))),
+            select(func.count())
+            .select_from(Applications)
+            .where(Applications.status.in_(active_status))
+        ),
         "interview": db.session.scalar(
-            select(func.count()).select_from(Interviews).where(Interviews.interview_date >= datetime.now())),
+            select(func.count())
+            .select_from(Interviews)
+            .where(Interviews.interview_date >= datetime.now())
+        ),
         "success": db.session.scalar(
-            select(func.count()).select_from(Applications).where(Applications.status == "内定")),
+            select(func.count())
+            .select_from(Applications)
+            .where(Applications.status == "内定")
+        ),
     }
 
-    return render_template("dashboard.html", stats=stats,applications=applications,interviews=interviews,bg_color=BADGE_COLOR)
+    return render_template(
+        "dashboard.html",
+        stats=stats,
+        applications=applications,
+        interviews=interviews,
+        bg_color=BADGE_COLOR,
+    )
